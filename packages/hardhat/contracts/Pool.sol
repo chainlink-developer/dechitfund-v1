4b595508622a12538ca7e5dc985a9f3a92d37cc2
Sebastian T F <sebastian.tharakan97@gmail.com>
2021-07-05 15:15:29 +0530

    add Pool.join()

--- packages/hardhat/contracts/Pool.sol
+++ packages/hardhat/contracts/Pool.sol
@@ -21,6 +21,8 @@ contract Pool {
   uint256 public maxBidPercent;
   uint256 public currentTerm;
   State public state;
+  address[] public members;
+  mapping(address => bool) public isMember;
 
   constructor(
     uint256 _noOfMembersNoOfTerms,
@@ -35,5 +37,17 @@ contract Pool {
     instalmentAmount = _instalmentAmount;
     maxBidPercent = _maxBidPercent;
     state = State.OPEN;
+  }
+
+  function join() public {
+    require(state == State.OPEN, "Pool State is not OPEN");
+    require(
+      members.length != noOfMembersNoOfTerms,
+      "Pool already has required no. of members"
+    );
+    require(!isMember[msg.sender], "Caller is already a member");
+    members.push(msg.sender);
+    isMember[msg.sender] = true;
+    if (members.length == noOfMembersNoOfTerms) state = State.STARTED;
   }
 }
--- packages/hardhat/test/Pool.test.js
+++ packages/hardhat/test/Pool.test.js
@@ -5,6 +5,10 @@ const { solidity } = require("ethereum-waffle");
 use(solidity);
 
 describe("DeChitFund v1", function () {
+  let accounts;
+  let deployer;
+  let wallet1;
+
   let poolContract;
 
   const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
@@ -15,6 +19,12 @@ describe("DeChitFund v1", function () {
     STARTED: 1,
     COMPLETE: 2,
   };
+
+  before(async () => {
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    wallet1 = accounts[1];
+  });
 
   describe("Pool", function () {
     it("Should deploy and initialize Pool", async function () {
@@ -42,6 +52,45 @@ describe("DeChitFund v1", function () {
       expect(await poolContract.instalmentAmount()).to.equal(args.instalmentAmount);
       expect(await poolContract.maxBidPercent()).to.equal(args.maxBidPercent);
       expect(await poolContract.state()).to.equal(poolStates.OPEN);
+    });
+
+    it("Should allow non-members to join Pool", async function () {
+      await poolContract.connect(wallet1).join();
+
+      expect(await poolContract.members(0)).to.equal(wallet1.address);
+      expect(await poolContract.isMember(wallet1.address)).to.equal(true);
+    });
+
+    it("Should disallow existing members to join Pool", async function () {
+      await expect(poolContract.connect(wallet1).join()).to.be.revertedWith(
+        "Caller is already a member"
+      );
+    });
+
+    it("Should start Pool when last member has joined Pool", async function () {
+      await poolContract.connect(accounts[2]).join();
+      await poolContract.connect(accounts[3]).join();
+      await poolContract.connect(accounts[4]).join();
+      await poolContract.connect(accounts[5]).join();
+      await poolContract.connect(accounts[6]).join();
+      await poolContract.connect(accounts[7]).join();
+      await poolContract.connect(accounts[8]).join();
+      await poolContract.connect(accounts[9]).join();
+      await poolContract.connect(accounts[10]).join();
+
+      expect(await poolContract.state()).to.equal(poolStates.STARTED);
+    });
+
+    it("Should disallow new members to join when Pool State is not OPEN", async function () {
+      await expect(poolContract.connect(accounts[11]).join()).to.be.revertedWith(
+        "Pool State is not OPEN"
+      );
+    });
+
+    it.skip("Should disallow new members to join when Pool already has required no. of members", async function () {
+      await expect(poolContract.connect(accounts[11]).join()).to.be.revertedWith(
+        "Pool already has required no. of members"
+      );
     });
   });
 });
